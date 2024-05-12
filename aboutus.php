4482bd14e7ccaf2cf27b32dc2a8d69e0699948c7
SIVAJB <121388913+sivajeyabalan@users.noreply.github.com>
2024-05-12 21:20:58 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>ODYSSSEY 2k24</title>
+<?php require 'utils/styles.php'; ?><!--css links. file found in utils folder-->
+</head>
+<style>
+
+/* Large rounded green border */
+hr.blueline {
+  border: 10px solid #00004d;
+  border-radius: 5px;
+}
+
+#bj
+{
+  font-size: 22px;
+}
+</style>
+
+<body style="background-color:#b3e0ff ">
+
+  <?php require 'utils/header.php'; ?>
+  <hr class="blueline">
+  <div class="col-md-12">
+  
+<h1>About Us</h1>
+
+<p id="bj"> Velammal College of Engineering and Technology</p>
+
+<p id="bj"></p>
+
+<p id="bj"> ODYSSSEY is a Technical fest which is held every year, we like everyone to participate cheerfully in every event.</p>
+
+</div>
+<hr class="blueline">
+</body>
+
+ <?php require 'utils/footer.php'; ?>
+
+</html>
